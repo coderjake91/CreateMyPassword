2336905ea764f15e09f626244e39f23eaf086701
coderjake91 <jacob.b.frazer@gmail.com>
2021-09-12 01:52:35 -0500

    added user prompt method to capture user password generation preferences

--- assets/js/Password.js
+++ assets/js/Password.js
@@ -6,11 +6,32 @@
     // }
 
 let passLength = 50;
+let passPreferences = [];
 
 //method that prompts user and constructs password preferences
 
 function buildPassword(){
+    window.prompt("Welcome to CreateMyPassword! Let's step through a few password preferences in order to generate your new password...");
+}
 
+function passPrompt(){
+    
+    passLength = window.prompt('What length would you like your password to be? (choose a number between 8 and 128 characters');
+
+    passLength = parseInt(passLength);
+
+        while(!Number.isSafeInteger(passLength) || passLength > 128 || passLength < 8){
+            window.alert('You have not correctly chosen a password length...');
+            passPrompt();
+        }
+
+    passPreferences[0] = window.confirm('Would you like to include lowercase characters?');
+    passPreferences[1] = window.confirm('Would you like to include uppercase characters?');
+    passPreferences[2] = window.confirm('Would you like to include numeric characters?');
+    passPreferences[4] = window.confirm('Would you like to include special characters?');
+
+    console.log(passLength);
+    passPreferences.forEach(preference => console.log(preference));
 }
 
 //method that produces an array of random integers between 0 - 128 using crypto API
@@ -38,8 +59,9 @@ console.log(getRandEightBitIntArr());
 console.log(String.fromCodePoint(33));
 
 //method that accepts random int array and converts it to a password with user specified characters
-function convertRandIntArrToPass(){
-
+function convertRandIntArrToPass(randomIntArray){
+    
+    
 }
 
 
